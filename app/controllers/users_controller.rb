7dc40ba5d52e5b4f6b7494522a628436ce068861
Shinobu Sugimoto <hoshifuruokani@gmail.com>
2016-11-03 07:44:16 +0000

    add sign up and user detail

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,26 @@
+class UsersController < ApplicationController
+  
+  def show
+    @user = User.find(params[:id])
+  end
+  
+  def new
+    @user = User.new # Userクラスの新しいインスタンスを作成して、UsersControllerのインスタンス変数@userに代入
+  end
+  
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to the Sample App!" #flashというハッシュを作成してmessage_typeキーにsuccessを、messageにWelcome to the Sample App!をいれている
+      redirect_to @user # redirect_to user_path(@user)と同様
+    else
+      render "new"
+    end
+  end
+    
+    private
+    
+  def user_params
+    params.require(:user).permit(:name, :sexes, :email, :password, :password_confirmation)
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,12 @@
 class User < ActiveRecord::Base
+    before_save {self.email = self.email.downcase } #データ保存前に小文字に変換
+    validates :name, presence: true, length: {maximum: 50 }
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    validates :email, presence: true, length: {maximum: 255 },format: {with: VALID_EMAIL_REGEX },uniqueness: { case_sensitive: false }
+    has_secure_password
+=begin
+    ・データベースに安全にハッシュ化（暗号化）されたpassword_digestを保存する。
+    ・passwordとpassword_confirmationをモデルに追加して、パスワードの確認が一致するか検証する。
+    ・パスワードが正しいときに、ユーザーを返すauthenticateメソッドを提供する。
+=end
 end
