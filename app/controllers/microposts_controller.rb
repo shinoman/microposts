9a77eecbf03d0d8305b9180084163dabb0a95532
Shinobu Sugimoto <hoshifuruokani@gmail.com>
2016-11-04 11:43:37 +0000

    フォームスタイル変更

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -3,16 +3,27 @@ class MicropostsController < ApplicationController
     
     def create
         @microposts = current_user.microposts.build(micropost_params)
-        if @micropost.save
+        if @microposts.save
             flash[:success] = "Micropost created!"
             redirect_to root_url
         else
             render "static_pages/home"
         end
     end
+=begin
+パラメータを受け取って現在のユーザーに紐付いたMicropostのインスタンスを作成して@micropost変数に入れ、@micropost.saveで保存が成功した場合は、
+root_urlである/にリダイレクトを行い、失敗した場合はapp/views/static_pages/home.html.erbのテンプレートを表示します。
+=end
     
     private
     def micropost_params
         params.require(:micropost).permit(:content)
     end
+=begin
+micropost_paramsでは、フォームから受け取ったパラメータのparams[:micropost]のうち、
+params[:micropost][:content]のみデータの作成に使用するようにStrong Parametersを宣言しています。
+コントローラでNote.new(params[:note])という書き方をしてしまうとどんな値でもセットできてしまうようになるのでセキュリティ上の問題がある。
+そのため保存を許可するカラムを指定する「ストロングパラメータ」というものを使う。requireの引数にparamsのキー、permitの引数に保存を許可するカラムを指定する。 
+そして、許可したカラムのみのハッシュを返すメソッドをprivateメソッドとして定義する。 
+=end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,8 @@ class UsersController < ApplicationController
   before_action :collation_user, only:[:edit, :update]
   
   def show
+    @user = User.find(params[:id])
+    @microposts = @user.microposts.order(created_at: :desc) #ユーザーに紐付いたマイクロポストを作成日時が新しいものから取得し、@micropostsに代入しています。
   end
   
   def new
